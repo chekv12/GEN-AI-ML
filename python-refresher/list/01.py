7b017e6b84bcde8cf3b14977650ce3fb0f378e36
xcheche <chekwubechukwu23@gmail.com>
2025-12-09 19:27:21 +0100

    Done

---
+++ python-refresher/list/01.py
@@ -0,0 +1,10 @@
+my_list  = [1,2,3,4,5,6,7]
+print(my_list)
+insert_new = my_list.insert(1,4)
+print(my_list)
+#--------------CHange----------
+change = my_list[0] = 'Orange'
+print(my_list)
+#---------------List slicing----------------
+sliced = my_list[:3]
+print(sliced)
